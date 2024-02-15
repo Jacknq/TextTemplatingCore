16356508c55586d9a10d12165eb4026b113f2469
jacknq <bousekr@gmail.com>
2024-02-15 18:21:09 +0100

    upgrade extension

--- src/vsextention/tt/texttemplating/README.md
+++ src/vsextention/tt/texttemplating/README.md
@@ -5,7 +5,7 @@
 **Execute from context menu from VSCode!**
 
 One of the very few tools that supports reflection and **web** projects!
-Compatible with **.NET 6 & .NET Core 3.1**  cs projects
+Compatible with **.NET Core 7+**  cs projects
 
 Scaffolding for .NET Core without IDE. Command line tool and designtime transformation tool, based on roslyn, **no mono or 3rdparty dependencies**.
 
@@ -19,7 +19,7 @@ Generate **any** text, **any** code or script template, **any** language from yo
 ![features](https://raw.githubusercontent.com/Jacknq/TextTemplatingCore/master/src/vsextention/texttemplating/images/vscontextt.png)
 
 ## Requirements
- 1) Install .NET Core SDK 3.1+ or higher  https://dotnet.microsoft.com/download/dotnet-core
+ 1) Install .NET SDK 7+ or higher  https://dotnet.microsoft.com/download/dotnet-core
  2) Install textemplating tool 
  ### Install locally - inside project folder
 ```
@@ -47,39 +47,3 @@ dotnet tt Person.tt
  
 dotnet tt trans -f Person.tt
 ```
-<!-- 
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension. -->
-
-
-
-## Release Notes 
- Github repo
- https://github.com/Jacknq/TextTemplatingCore
-
-<!-- ## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/) -->
-
-**Enjoy!**
---
+++ src/vsextention/tt/texttemplating/out/extension.js
@@ -0,0 +1,124 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = __importStar(require("vscode"));
+const path = __importStar(require("path"));
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    let NEXT_TERM_ID = 1;
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "texttemplating" is now active!');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('tt.helloWorldOLD', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Hello World from texttemplating!');
+    });
+    context.subscriptions.push(vscode.commands.registerCommand('tt.helloWorld', (uri) => {
+        const terminals = vscode.window.terminals;
+        const items = terminals.map(t => {
+            return {
+                label: `${t.name}`,
+                terminal: t
+            };
+        });
+        const nname = `TT #${path.basename(uri.fsPath)}`;
+        var terminal;
+        var f = items.find(v => v.label == nname);
+        if (f != null) {
+            terminal = f.terminal;
+        }
+        else {
+            terminal = vscode.window.createTerminal({
+                name: nname,
+                cwd: path.dirname(uri.fsPath)
+            });
+        }
+        //	`TT #${path.basename(uri.fsPath)}`,path.dirname(uri.fsPath), ['-l']);
+        terminal.sendText(`dotnet tt ${path.basename(uri.fsPath)}`);
+        if (f == null) {
+            terminal.show();
+        }
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('terminalTest.show', () => {
+        if (ensureTerminalExists()) {
+            selectTerminal().then(terminal => {
+                if (terminal) {
+                    terminal.show();
+                }
+            });
+        }
+    }));
+    //https://github.com/microsoft/vscode-extension-samples/blob/master/terminal-sample/src/extension.ts
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+function colorText(text) {
+    let output = '';
+    let colorIndex = 1;
+    for (let i = 0; i < text.length; i++) {
+        const char = text.charAt(i);
+        if (char === ' ' || char === '\r' || char === '\n') {
+            output += char;
+        }
+        else {
+            output += `\x1b[3${colorIndex++}m${text.charAt(i)}\x1b[0m`;
+            if (colorIndex > 6) {
+                colorIndex = 1;
+            }
+        }
+    }
+    return output;
+}
+function selectTerminal() {
+    const terminals = vscode.window.terminals;
+    const items = terminals.map(t => {
+        return {
+            label: `name: ${t.name}`,
+            terminal: t
+        };
+    });
+    return vscode.window.showQuickPick(items).then(item => {
+        return item ? item.terminal : undefined;
+    });
+}
+function ensureTerminalExists() {
+    if (vscode.window.terminals.length === 0) {
+        vscode.window.showErrorMessage('No active terminals');
+        return false;
+    }
+    return true;
+}
+//# sourceMappingURL=extension.js.map
